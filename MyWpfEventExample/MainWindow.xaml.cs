df45a891f460ae92aebf1d04836c4a3eb9dfc05e
Uwe Walter <uhw@gmx.org>
2020-11-23 22:03:05 +0100

    Start.

---
+++ MyWpfEventExample/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows;
+
+namespace MyWpfEventExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private delegate void TheCall();
+
+        TheCall _theCall;
+
+        private Window _toolWindow;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _toolWindow = CreateToolWindow();
+            _theCall = new TheCall(MyBeep);
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        /// <summary>
+        /// Button_Click_ToolWindow
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_ToolWindow(object sender, RoutedEventArgs e)
+        {
+            _toolWindow.Show();
+            _toolWindow.Owner = this;
+        }
+
+        /// <summary>
+        /// Button_Click_Close
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+        
+        /// <summary>
+        /// BeepEventHandler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BeepEventHandler(object sender, EventArgs e)
+        {
+            _theCall();
+        }
+        private void MyBeep()
+        {
+            Console.Beep();
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// CreateToolWindow
+        /// </summary>
+        /// <returns></returns>
+        private Window CreateToolWindow()
+        {
+            ToolWindow toolWindow = new ToolWindow();
+            toolWindow.WindowStyle = WindowStyle.ToolWindow;
+            toolWindow.ShowInTaskbar = false;
+            toolWindow.BeepEvent += BeepEventHandler;
+
+            return toolWindow;
+        }
+
+        #endregion
+
+    }
+}
---
+++ MyWpfEventExample/ToolWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+
+namespace MyWpfEventExample
+{
+    /// <summary>
+    /// Interaction logic for ToolWindow.xaml
+    /// </summary>
+    public partial class ToolWindow : Window
+    {
+        public event EventHandler BeepEvent;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public ToolWindow()
+        {
+            InitializeComponent();
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        /// <summary>
+        /// Button_Click_FireEvent
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_FireEvent(object sender, RoutedEventArgs e)
+        {
+            BeepEvent?.Invoke(sender, e);
+        }
+
+        /// <summary>
+        /// Button_Click_Close
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_Close(object sender, RoutedEventArgs e)
+        {            
+            Hide();
+        }
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        #endregion
+    }
+}
